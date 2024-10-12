e38a2b5a13f21e2b48a436c15acadec6d9759066
lubro0 <142106680+lubro0@users.noreply.github.com>
2024-10-12 15:12:55 +0200

    Create Main.php

---
+++ src/Main.php
@@ -0,0 +1,40 @@
+<?php
+
+declare(strict_types=1);
+
+namespace gmjava\GamemodeJava;
+
+use pocketmine\plugin\PluginBase;
+use pocketmine\command\Command;
+use pocketmine\command\CommandSender;
+use pocketmine\player\Player;
+
+class Main extends PluginBase{
+
+    public function onEnable(): void {
+        $this->getLogger()->info("GamemodeJava enabled");
+    }
+
+    public function onCommand(CommandSender $sender, Command $command, string $label, array $args): bool {
+        if ($sender instanceof Player && $sender->hasPermission("gm.use")) {
+            switch ($command->getName()) {
+                case "gm":
+                    if (isset($args[0])) {
+                        switch ($args[0]) {
+                            case "1":
+                                $sender->setGamemode(1);
+                                break;
+                            case "2":
+                                $sender->setGamemode(0);
+                                break;
+                            case "3":
+                                $sender->setGamemode(3);
+                                break;
+                        }
+                    }
+                    return true;
+            }
+        }
+        return false;
+    }
+}
