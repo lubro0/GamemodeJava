9bccb147d4b688ec778b3a6b15dba88a90732c20
lubro0 <142106680+lubro0@users.noreply.github.com>
2024-10-12 15:28:44 +0200

    Update Main.php

--- src/Main.php
+++ src/Main.php
@@ -8,27 +8,33 @@ use pocketmine\plugin\PluginBase;
 use pocketmine\command\Command;
 use pocketmine\command\CommandSender;
 use pocketmine\player\Player;
+use pocketmine\player\GameMode;
 
 class Main extends PluginBase{
 
+    public function onEnable(): void {
+        $this->getServer()->getCommandMap()->unregister($this->getServer()->getCommandMap()->getCommand("gamemode"));
+    }
+
     public function onCommand(CommandSender $sender, Command $command, string $label, array $args): bool {
         if ($sender instanceof Player && $sender->hasPermission("gm.use")) {
-            switch ($command->getName()) {
-                case "gm":
-                    if (isset($args[0])) {
-                        switch ($args[0]) {
-                            case "1":
-                                $sender->setGamemode(1);
-                                break;
-                            case "2":
-                                $sender->setGamemode(0);
-                                break;
-                            case "3":
-                                $sender->setGamemode(3);
-                                break;
-                        }
+            if ($command->getName() === "gm") {
+                if (isset($args[0])) {
+                    switch ($args[0]) {
+                        case "1":
+                            $sender->setGamemode(GameMode::CREATIVE());
+                            return true;
+                        case "2":
+                            $sender->setGamemode(GameMode::SURVIVAL());
+                            return true;
+                        case "3":
+                            $sender->setGamemode(GameMode::SPECTATOR());
+                            return true;
+                        default:
+                            $sender->sendMessage("§cInvalid Gamemode!");
+                            return true;
                     }
-                    return true;
+                }
             }
         }
         return false;
